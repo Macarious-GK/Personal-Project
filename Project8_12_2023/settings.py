84afe7633c74c5552e09e973464b7c3b96035871
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2023-08-12 01:45:11 +0300

    Fixing static routing

--- Project8_12_2023/settings.py
+++ Project8_12_2023/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'Project8_12_2023.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Koko_Krkr/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,8 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = 'Koko_Krkr/static/'
+STATICFILES_DIRS = [
+    "Koko_Krkr/static",
+]
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
